9009fcf98ef8210af53fe742af6bbcec10bf425d
ciaramicaelag <125450878+ciaramicaelag@users.noreply.github.com>
2023-11-29 22:46:09 +0800

    Update Cache.java
    updated "calculateOutputs" for a better formula for avgMemoryAccessTime and totalMemoryAccessTime. Not entirely sure if correct, though.

--- mvc/Cache.java
+++ mvc/Cache.java
@@ -107,12 +107,13 @@ public class Cache {
     }
 
     public String calculateOutputs(){
-        hitRate = hitCount / (hitCount + missCount);
-        missRate = missCount / (missCount + hitCount);
+        int totalCount = hitCount + missCount;
+        hitRate = hitCount / totalCount;
+        missRate = missCount / totalCount;
 
         // CHANGE TO PROPER FORMULA
-        avgMemoryAccessTime = hitRate;
-        totalMemoryAccessTime = missRate;
+        avgMemoryAccessTime = hitCount / totalCount + (1 + cacheLine * 10 + 1 * missCount) / totalCount;
+        totalMemoryAccessTime = avgMemoryAccessTime * blockCount;
 
         return String.format("Hit Rate: %.2f\nMiss Rate: %.2f\nAvg Memory Access Time: %.2f\nTotal Memory Access Time: %.2f",
                             hitRate, missRate, avgMemoryAccessTime, totalMemoryAccessTime);
